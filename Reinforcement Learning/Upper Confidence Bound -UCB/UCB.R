a636acdf7c091871df28c8fa63f83be6df689775
Darshan Patel <29084562+darshanpatel1992@users.noreply.github.com>
2018-02-19 13:18:37 -0500

    Create UCB.R

---
+++ Reinforcement Learning/Upper Confidence Bound -UCB/UCB.R
@@ -0,0 +1,51 @@
+# Upper Confidence Bound
+#Importing  dataset
+
+dataset = read.csv('Ads_CTR_Optimisation.csv')
+
+# Implementing UCB
+N =10000
+d = 10
+numbers_of_selection = integer(d)
+sums_of_rewards = integer(d)
+ads_selected = integer()
+total_reward = 0
+for (n in 1:N) {
+  ad = 0
+  max_upper_bound =0
+    for (i in 1:d) {
+      if(numbers_of_selection[i]>0){
+        
+    average_rewards = sums_of_rewards[i]/numbers_of_selection[i]
+    delta_i = sqrt(3/2*log(n)/numbers_of_selection[i])
+    upper_bound = average_rewards + delta_i
+  }else{
+    upper_bound=1e400
+  }
+  
+    if(upper_bound> max_upper_bound){
+    max_upper_bound= upper_bound
+    ad= i
+    }
+  
+  }
+  
+  ads_selected = append(ads_selected,ad)
+  numbers_of_selection[ad] = numbers_of_selection[ad] + 1
+reward= dataset[n,ad]
+  sums_of_rewards[ad] = sums_of_rewards[ad] +reward
+  total_reward= total_reward + reward
+  
+    }    
+
+#visullising the histogram
+hist(ads_selected,
+     col = 'blue',
+     main = "Histogram of ads Selected",
+     xlab = 'ads',
+     ylab= ' No of ads select each time')
+
+
+
+
+
